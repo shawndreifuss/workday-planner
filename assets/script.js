66bb62cd753c2e6ecab2b7efabf5ba9624872d28
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-09-19 17:29:16 -0700

    added classes of past present future due to time of day

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,8 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 var today = dayjs()
-
-
+var saveBtn = $('saveBtn')
+var hour = dayjs().format('HH');
 
 function displayTime(){
 var date = dayjs().format('dddd, MMMM D');
@@ -13,14 +13,28 @@ $('#time').text(time);
 }
 
 
-setInterval(displayTime,1000)
 
 
+$('.time-block').each(function () {
 
+  var timeDisplay = $(this).attr('id').split('-')[1];
 
+  if (hour == timeDisplay) {
+    $(this).addClass('present')
 
+  }
+  else if (hour < timeDisplay) {
+    $(this).removeClass('present')
+    $(this).addClass('future')
+    
+  }
+else if (hour > timeDisplay) { 
+  $(this).removeClass('future')
+  $(this).addClass('past')
+ 
+}
+});
 
-$(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -39,4 +53,13 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
+
+
+
+
+
+ setInterval(displayTime,1000)
+
+
+ 
