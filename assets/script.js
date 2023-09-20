20edee6072eb4d966457203834e2913801dce079
Shawn Dreifuss <shawndreifuss@icloud.com>
2023-09-19 18:46:50 -0700

    clean up code and comment

--- assets/script.js
+++ assets/script.js
@@ -1,55 +1,35 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-var today = dayjs()
-var saveBtn = $('.saveBtn')
-var hour = dayjs().format('HH');
-
-function displayTime(){
-var date = dayjs().format('dddd, MMMM D');
-$('#date').text(date);
-var time = dayjs().format('hh:mm:ss');
-$('#time').text(time);
+//variables for time
+var today = dayjs();
+var hour = dayjs().format("HH");
+//will display current time
+function displayTime() {
+  var date = dayjs().format("dddd, MMMM D");
+  $("#date").text(date);
+  var time = dayjs().format("hh:mm:ss");
+  $("#time").text(time);
 }
+// will find the time display from local storage and display it in the correct column of time and change the color
+//of the background
+$(".time-block").each(function () {
+  var timeDisplay = $(this).attr("id").split("-")[1];
 
-// function renderTasks(){
-//   var taskInput = localStorage.getItem('value')
-//   $('.description').text(taskInput)
-// }
+  var textEntry = localStorage.getItem(timeDisplay);
+  var textArea = $(this).find(".description");
+  textArea.val(textEntry);
 
-
-$('.time-block').each(function () {
-
-var timeDisplay = $(this).attr('id').split('-')[1];
-
-var textEntry = localStorage.getItem(timeDisplay);
-    var textArea = $(this).find(".description");
-    textArea.val (textEntry);
-
-    if (timeDisplay < hour){
-      $(this).find('.description').addClass('past');
-  }else if(timeDisplay == hour){
-      $(this).find('.description').addClass('present');
-  }else{
-      $(this).find('.description').addClass('future');
-   }
+  if (timeDisplay < hour) {
+    $(this).find(".description").addClass("past");
+  } else if (timeDisplay == hour) {
+    $(this).find(".description").addClass("present");
+  } else {
+    $(this).find(".description").addClass("future");
+  }
 });
 
-
-$(".saveBtn").on('click', function () {
-  
+$(".saveBtn").on("click", function () {
   var value = $(this).parent().find(".description").val();
   var key = $(this).parent().attr("id").split("-")[1];
   localStorage.setItem(key, value);
 });
 
- setInterval(displayTime,1000)
-
-
-
-
-
-
-
-
- 
+setInterval(displayTime, 1000);
